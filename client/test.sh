25da506ff4b31797f0361138725b1a6fb5e7ef85
luweijun <luweijun@jfgenius.com>
2026-08-20 20:16:44 +0800

    fix: tiny fix

---
+++ client/test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+
+#del发布文件
+rm -rf dist
+
+pnpm install
+# 构建
+pnpm build
+
+# cd 到构建输出的目录下
+cd dist
+cp index.html 404.html 
+touch .nojekyll
+
+
+git init;
+git checkout -b master
+git add -A;
+git commit -m 'deploy';
+git remote add lefter git@github.com:ave-lefter/ave-lefter.github.io.git
+git push -f lefter master:master;
+
+cd -
